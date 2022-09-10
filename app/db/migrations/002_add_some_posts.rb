88bf527779a28cfa069b6e1d7fb75bc08d5db1bf
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-09 20:08:59 -0400

    tried changing the order of creating posts and requiring posts

--- app/db/migrations/002_add_some_posts.rb
+++ app/db/migrations/002_add_some_posts.rb
@@ -1,16 +1,19 @@
 class AddSomePosts < Sequel::Migration
-  puts "migrated"
+  def up
+    Post.create(
+      title: "What is Lorem Ipsum?",
+      content: "Lorem Ipsum is a dummy text...",
+    )
+    Post.create(
+      title: "Why do we use it?",
+      content: "It is a long established fact that a reader...",
+    )
+    Post.create(
+      title: "Where does it come from?",
+      content: "Contrary to popular belief, Lorem Ipsum is not simply...",
+    )
+  end
 
-  Post.insert(
-    title: "What is Lorem Ipsum?",
-    content: "Lorem Ipsum is a dummy text...",
-  )
-  Post.create(
-    title: "Why do we use it?",
-    content: "It is a long established fact that a reader...",
-  )
-  Post.create(
-    title: "Where does it come from?",
-    content: "Contrary to popular belief, Lorem Ipsum is not simply...",
-  )
+  def down
+  end
 end
