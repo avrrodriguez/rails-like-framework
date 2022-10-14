4b08f488deaba8e7a61cfcea76cd675e400147ab
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-10-14 17:14:55 -0400

    Fixes create_table method so it properly creates a table, Adds drop, join, and get_schema methods to Functions class in db

--- app/db/functions/db_functions.rb
+++ app/db/functions/db_functions.rb
@@ -1,22 +1,30 @@
 require "sequel"
 
 class Functions
-  # def initialize(*table)
-  #   if table
-  #     @dataset = DB[:"#{table}"]
-  #   end
-  # end
+  def name(table_name)
+    @table_name = table_name
+  end
 
-  def create_table(table_name, options = {})
-    db_new_table = "CREATE TABLE #{table_name} (#{options[:name]} #{options[:name_type]} #{:comment} #{:comment_type})"
+  def create_table(table_name, *options)
+    db_new_table = "CREATE TABLE #{table_name} (#{options})"
     DB.run "#{db_new_table}"
     puts "#{table_name} table created"
   end
 
-  def join(table1, table2)
+  def drop
+    DB.drop_table @table_name.to_sym
+  end
+
+  def join(table1, table1_column, table2, table2_matching_column)
+    db_join_tables = "SELECT * FROM #{table1} INNER JOIN #{table2} ON #{table1_column}=#{table2_matching_column}"
+  end
+
+  def get_schema
+    puts DB.schema(@table_name.to_sym)
   end
 end
 
-puts "Creating new table in database"
+puts "Accessing table in database"
 table = Functions.new()
-table.create_table("comments", { name: "name", name_type: "string", comment: "comment", comment_type: "text" })
+table.name("comments")
+table.get_schema
